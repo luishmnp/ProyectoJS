dcaf4b7024e3043b83cf3f5521098a051556e779
Luis Huaman <luishmnp@gmail.com>
2026-09-18 08:13:36 -0500

    He modificado archivos JS

--- js/ejercicio_05.js
+++ js/ejercicio_05.js
@@ -6,8 +6,8 @@ const combustible = 100;
 let consumo = combustible / kilometros
 // FASE DE SALIDA
 console.log("Nombre del usuario: ", nombreUsuario );
-console.log("Kilometros Recorridos", kilometros , "Km");
-console.log("Combustible Total", combustible, "Lt");
-console.log("Consumo por kilometro", consumo, "Lt");
+console.log("Kilometros Recorridos:", kilometros , "Km");
+console.log("Combustible Total:", combustible, "Lt");
+console.log("Consumo por kilometro:", consumo, "Lt");
 
 
